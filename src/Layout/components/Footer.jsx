4c9057eac8e58df9ec71be692e2e1b93f4c0df37
flavioepimentel <flavio.ericksan@hotmail.com>
2023-11-27 15:02:43 -0300

    style: :lipstick: Corrigi margem do footer

--- src/Layout/components/Footer.jsx
+++ src/Layout/components/Footer.jsx
@@ -4,7 +4,7 @@ import { footerLinks, socialMedia } from "../../constants";
 
 const Footer = () => (
   <section className={`${styles.flexCenter} ${styles.paddingY} flex-col`}>
-    <div className={`${styles.flexStart} md:flex-row flex-col mb-8 w-full`}>
+    <div className={`${styles.flexStart} md:flex-row flex-col mb-8 w-full ml-4`}>
       <div className="flex-[1] flex flex-col justify-start mr-10">
         <img
           src={logo_black}
@@ -39,12 +39,12 @@ const Footer = () => (
       </div>
     </div>
 
-    <div className="w-full flex justify-between items-center md:flex-row flex-col pt-6 border-t-[1px] border-t-[#3F3E45]">
+    <div className="w-full flex justify-between items-center md:flex-row flex-col pt-6 border-t-[1px] border-t-[#3F3E45] ml-4">
       <p className="font-poppins font-normal text-center text-[18px] leading-[27px] text-black">
         Copyright Ⓒ 2022 Sciclope. Direitos Reservados.
       </p>
 
-      <div className="flex flex-row md:mt-0 mt-6">
+      <div className="flex flex-row md:mt-0 mt-6 mr-6">
         {socialMedia.map((social, index) => (
           <img
             key={social.id}

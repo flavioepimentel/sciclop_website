44dc5c74fb08ac584540a7ebdc70c106df6ec6dd
flavioepimentel <flavio.ericksan@hotmail.com>
2023-12-01 20:17:00 -0300

    refactor: :truck: Atualiza caminho para componentes globais

--- src/pages/Home/components/CardDeal.jsx
+++ src/pages/Home/components/CardDeal.jsx
@@ -1,6 +1,6 @@
 import { card } from "../../../assets";
 import styles, { layout } from "../../../style";
-import Button from "./Button";
+import Button from "../../../components/Button";
 
 const CardDeal = () => (
   <section className={`flex-1 flex justify-center items-start flex-col xl:px-0 sm:px-16 px-6 ${layout.section}`}>

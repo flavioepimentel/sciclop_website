bbe6cce9d5f2c0f8d8b449744a9af16306e7520b
flavioepimentel <flavio.ericksan@hotmail.com>
2023-12-01 20:07:14 -0300

    refactor: :building_construction: pROMOVE COMPONENTES AO NÍVEL GLOBAL

---
+++ src/components/ButtonReverse.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const ButtonReverse = ({ styles }) => (
+  <button type="button" className={`py-4 px-6 font-poppins font-medium text-[18px] text-black bg-white rounded-[10px] outline-none ${styles}`}>
+    Faça um Orçamento
+  </button>
+);
+
+export default ButtonReverse;

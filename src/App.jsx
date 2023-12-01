31d2ea8f5a4378eb6fe2b62b4984e23f01eadef5
flavioepimentel <flavio.ericksan@hotmail.com>
2023-12-01 20:08:18 -0300

    feat: :sparkles: Implementa 3 novas rotas
    Rotas para 3 categorias de módulos

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./Layout/Layout";
 import Home from "./pages/Home/Home";
-import Modulos from "./pages/Modulos/Modulos";
+import Financial from "./pages/Modulos/Financial";
+import Fleet from "./pages/Modulos/Fleet";
+import Maintenance from "./pages/Modulos/Maintenance";
 import About from "./pages/About/About";
 import NoPage from "./pages/NoPage/NoPage";
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -13,7 +15,9 @@ export default function App() {
         <Route exact path="/sciclop_website" element={<Layout />}>
           <Route exact index element={<Home />} />
           <Route exact path="about" element={<About />} />
-          <Route exact path="modulos" element={<Modulos />} />
+          <Route exact path="financial" element={<Financial />} />
+          <Route exact path="fleet" element={<Fleet />} />
+          <Route exact path="maintenance" element={<Maintenance />} />
           <Route exact path="*" element={<NoPage />} />
         </Route>
       </Routes>

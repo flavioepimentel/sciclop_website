8014ba575aa98c63fb2735de80633ab21e1653bb
flavioepimentel <flavio.ericksan@hotmail.com>
2023-12-01 20:14:03 -0300

    style: :speech_balloon: Add text

---
+++ src/pages/Modulos/Maintenance.jsx
@@ -0,0 +1,32 @@
+const Maintenance = () => {
+  return (
+  <> 
+  <h1>Manutenção</h1>
+
+  <section>
+    <h2>Pneus</h2>
+    <p>
+      Juntamente com combustíveis representa o maior percentual no custo operacional na maioria das atividades envolvendo frotas. Este módulo permite controlar o desempenho dos pneus com base em suas posições nos veículos, bem como nas aplicações dos equipamentos, fornecendo assim base de dados que culmina na possibilidade de definição de qual o melhor item para cada aplicação tendo por padrão o menor custo operacional. 
+      Da mesma forma, as companhias dedicadas ao reparo dos pneumáticos (as ressoladoras) têm o seu desempenho fiscalzado facilitando as negociações e permitindo a melhoria do processo de ressolagem tenho em vista a redução de custos pelo aumento da quilometragem.
+      Liga-se aos módulos de abastecimento, compras, almoxarifado, oficina.
+    </p>
+  </section>
+  <section>
+    <h2>Mecânica</h2>
+    <p>
+      Embora pareça redundante, este módulo tem por finalidade o acompanhamento de itens finos do processo mecânico quando for de interesse do cliente. Caso exista ou a empresa resolva por montar por exemplo uma sala de motores destinada a desmontar o componente, analisar a sua falha determinando e controlando a prestação de serviços de retíficas, com posterior montagem e testes ou uma sala dedicada à manutençao de sistemas hidráulicos como comandos, transmissões e conversores de torque, aqui estarão os recursos para registro e controle desse processo.
+      Liga-se aos módulos de abastecimento, almoxarifado, compras, oficina.
+    </p>
+  </section>
+  <section>
+    <h2>Material rodante</h2>
+    <p>
+      Semelhante ao item pneus, o material rodante de tratores sobre esteiras, representa um dos maiores itens de custo operacional para esse tipo de equipamento.
+      Tido como um sistema bruto, o aprofundamento no assunto revela um item bastante delicado em seu trato relativo a custos onde um pequeno desajuste redunda em um aumento considerável nas despesas por recuperação e pela redução do tempo de produção. Assim medições e acompanhamento de desgastes podem previnir problemas, bem como recuperaçoes feitas de forma técnica, auxiliarão o atingimento de uma quantidade consideravelmente maior do número de horas trabalhadas sem paradas desnecessárias.
+    </p>
+  </section> 
+  </>
+  )
+};
+
+export default Maintenance;

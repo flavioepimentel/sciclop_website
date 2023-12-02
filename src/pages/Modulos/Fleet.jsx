e8920ae1671dd451d90b44c2f32273fea174f239
flavioepimentel <flavio.ericksan@hotmail.com>
2023-12-01 22:55:55 -0300

    style: :lipstick: Add style layout

--- src/pages/Modulos/Fleet.jsx
+++ src/pages/Modulos/Fleet.jsx
@@ -1,24 +1,53 @@
+import styles, { layout } from "../../style";
+import { notebook_mocukp } from "../../assets";
+import  CTA  from "../../components/CTA"
+import  Button  from "../../components/Button"
+
 const Modulos = () => {
   return (
   <> 
   <h1>Frotas</h1>
-  <section id="modulos">
-    <h2 className="font-poppins font-semibold xs:text-[48px] text-[40px] text-black xs:leading-[76.8px] leading-[66.8px] w-full">Veículos</h2>
+  <section className={layout.sectionReverse}>
+    <div className={layout.sectionInfo}>
+    <h2 className={styles.heading2}>
+      Veículos</h2>
       <p className="font-poppins font-normal text-black text-[18px] leading-[30.8px] max-w-[470px] mt-5">
         Contém todas as iformações cadastrais da frota sob controle do sistema. Armazena também os dados básicos de operação de grande parte dos módulos operantes.
         Tem as ligações para cada uma das funções de controle de recursos da frota como combustíveis, pneus, filtros e lubrificantes, combustíveis etc. É a base de operação de todo o sistema.
       </p>
+      </div>
+
+    <div className={layout.sectionImgReverse}>
+      <img src={notebook_mocukp} alt="billing" className="w-[100%] h-[100%] relative z-[5]" />
+      {/* gradient start */}
+      <div className="absolute z-[3] -left-1/2 top-0 w-[50%] h-[50%] rounded-full white__gradient" />
+      <div className="absolute z-[0] w-[50%] h-[50%] -left-1/2 bottom-0 rounded-full pink__gradient" />
+      {/* gradient end */}
+    </div>
   </section>
-  <section>
-    <h2 className="font-poppins font-semibold xs:text-[48px] text-[40px] text-black xs:leading-[76.8px] leading-[66.8px] w-full">Postos</h2>
+      <section className={layout.sectionReverse}>
+    <div className={layout.sectionImgReverse}>
+      <img src={notebook_mocukp} alt="billing" className="w-[100%] h-[100%] relative z-[5]" />
+
+      {/* gradient start */}
+      <div className="absolute z-[3] -left-1/2 top-0 w-[50%] h-[50%] rounded-full white__gradient" />
+      <div className="absolute z-[0] w-[50%] h-[50%] -left-1/2 bottom-0 rounded-full pink__gradient" />
+      {/* gradient end */}
+    </div>
+    <div className={layout.sectionInfo}>
+    <h2 className={styles.heading2}>
+      Postos
+      </h2>
       <p className="font-poppins font-normal text-black text-[18px] leading-[30.8px] max-w-[470px] mt-5">
         Os clientes que tiverem em suas estruturas operacionais postos próprios de abastecimento, terão em mãos todos os dados táticos e estratégicos deste recurso, pois lida com um dos itens de maior relevância na estrutura de custos operacionais que é o combustível.
         Garantir a utilização ótima deste recurso torna-se de extrema valia. O módulo acompanha os estoques em cada tanque da empresa, planeja e avisa sobre eventos como conferência física de estoque, aferição de bombas de abastecimento, além de acompanhar a qualidade do material que chega dos distribuidores, avisando previamente em caso de irregularidades.
         Liga-se ao módulo de materiais (almoxarifado) e ao de abastecimento.
       </p>
+    </div>
   </section>
-  <section>
-    <h2 className="font-poppins font-semibold xs:text-[48px] text-[40px] text-black xs:leading-[76.8px] leading-[66.8px] w-full">
+    <section className={layout.sectionReverse}>
+    <div className={layout.sectionInfo}>
+    <h2 className={styles.heading2}>
       Abastecimentos
       </h2>
     <p className="font-poppins font-normal text-black text-[18px] leading-[30.8px] max-w-[470px] mt-5">
@@ -26,9 +55,27 @@ const Modulos = () => {
       Fornece informações sobre ocorrência de excessos que possibilitam ações corretivas sejam de origem mecânica ou operacional.
       Liga-se aos módulos de postos, veículos, materiais (almoxarifado), lubrificação e a todos os demais módulos, pois como dito, dá origem ao principal suporte do sistema que é a quilometragem ou horas acumuladas.
     </p>
+  </div>
+  <div className={layout.sectionImgReverse}>
+      <img src={notebook_mocukp} alt="billing" className="w-[100%] h-[100%] relative z-[5]" />
+
+      {/* gradient start */}
+      <div className="absolute z-[3] -left-1/2 top-0 w-[50%] h-[50%] rounded-full white__gradient" />
+      <div className="absolute z-[0] w-[50%] h-[50%] -left-1/2 bottom-0 rounded-full pink__gradient" />
+      {/* gradient end */}
+    </div>
   </section>
-  <section>
-    <h2 className="font-poppins font-semibold xs:text-[48px] text-[40px] text-black xs:leading-[76.8px] leading-[66.8px] w-full">
+   <section className={layout.sectionReverse}>
+    <div className={layout.sectionImgReverse}>
+      <img src={notebook_mocukp} alt="billing" className="w-[100%] h-[100%] relative z-[5]" />
+
+      {/* gradient start */}
+      <div className="absolute z-[3] -left-1/2 top-0 w-[50%] h-[50%] rounded-full white__gradient" />
+      <div className="absolute z-[0] w-[50%] h-[50%] -left-1/2 bottom-0 rounded-full pink__gradient" />
+      {/* gradient end */}
+    </div>
+    <div className={layout.sectionInfo}>
+    <h2 className={styles.heading2}>
       Lubrificação
       </h2>
     <p className="font-poppins font-normal text-black text-[18px] leading-[30.8px] max-w-[470px] mt-5">
@@ -36,18 +83,38 @@ const Modulos = () => {
       Para isso todos os compartimentos são cadastrados com suas características de capacidade, tipo de óleo e períodos de intervenção. Com as informações fornecidas pelo módulo de abastecimento (a quilometragem ou horas), o sistema escala as trocas de óleo de filtros e uma vez integrado ao módulo de materiais, já requisita os filtros que serão utlizados em cada equipamento vencido e encaminha as ordens de troca para os responsáveis determinados.
       Liga-se ao módulo de veículos, abastecimento, materiais, custos.
     </p>
+    </div>
   </section>
-  <section>
-    <h2 className="font-poppins font-semibold xs:text-[48px] text-[40px] text-black xs:leading-[76.8px] leading-[66.8px] w-full">
+  <section className={layout.sectionReverse}>
+    <div className={layout.sectionInfo}>
+    <h2 className={styles.heading2}>
       Laboratório
       </h2>
     <p className="font-poppins font-normal text-black text-[18px] leading-[30.8px] max-w-[470px] mt-5">
       A análise de óleo lubrificante retirada de um compartimento, fornece informações preventivas importantíssimas sobre o estado do compartimento, da mesma maneira que um exame de sangue fornece dados sobre a nossa saúde. Uma vez implantado em calibrado, permite acompanhar ocorrências mecânicas ou operacionais que podem reduzir significativamente a vida do compartimento, como a entrada de poeira em um motor ou a operação com constante superaquecimento ou um débito anormal de diesel por falha em bicos ou bomba injetora mesmo um desgaste excessivo nas engrenagens ou rolamentos de uma caixa de mudanças e muito mais.
       Liga-se aos módulos de abastecimento, mecânica, custos.
     </p>
+    </div>
+    <div className={layout.sectionImgReverse}>
+      <img src={notebook_mocukp} alt="billing" className="w-[100%] h-[100%] relative z-[5]" />
+
+      {/* gradient start */}
+      <div className="absolute z-[3] -left-1/2 top-0 w-[50%] h-[50%] rounded-full white__gradient" />
+      <div className="absolute z-[0] w-[50%] h-[50%] -left-1/2 bottom-0 rounded-full pink__gradient" />
+      {/* gradient end */}
+    </div>
   </section>
-  <section>
-    <h2 className="font-poppins font-semibold xs:text-[48px] text-[40px] text-black xs:leading-[76.8px] leading-[66.8px] w-full">
+  <section className={layout.sectionReverse}>
+    <div className={layout.sectionImgReverse}>
+      <img src={notebook_mocukp} alt="billing" className="w-[100%] h-[100%] relative z-[5]" />
+
+      {/* gradient start */}
+      <div className="absolute z-[3] -left-1/2 top-0 w-[50%] h-[50%] rounded-full white__gradient" />
+      <div className="absolute z-[0] w-[50%] h-[50%] -left-1/2 bottom-0 rounded-full pink__gradient" />
+      {/* gradient end */}
+    </div>
+    <div className={layout.sectionInfo}>
+    <h2 className={styles.heading2}>
       Oficina
       </h2>
     <p className="font-poppins font-normal text-black text-[18px] leading-[30.8px] max-w-[470px] mt-5">
@@ -55,7 +122,9 @@ const Modulos = () => {
       Como o sistema prevê a criação de planos de manutençao preventiva, o histórico criado por esse módulo associado à informação da quilometragem/horas das quebras, gera um processo de aperfeiçoamento da preventiva que, com o decorrer do tempo, tende a reduzir drasticamente as corretivas e socorros em campo.
       Liga-se aos módulos de almoxarifado, abastecimento, custos, compras, administração, laboratório.
     </p>
+    </div>
   </section>
+  <CTA />
   </>
   )
 };

ce68615f6e5f46ea40613d16b527badb92243827
flavioepimentel <flavio.ericksan@hotmail.com>
2023-12-04 11:12:51 -0300

    style: :lipstick: Alinha posição texto com alinhamento de imagem

--- src/pages/Modulos/Financial.jsx
+++ src/pages/Modulos/Financial.jsx
@@ -1,5 +1,6 @@
 import styles, { layout } from "../../style";
 import { notebook_mocukp } from "../../assets";
+import { adm, almoxarifado } from "./assets";
 import  CTA  from "../../components/CTA"
 import  Button  from "../../components/Button"
 
@@ -7,6 +8,14 @@ const Financial = () => {
   return (
   <> 
     <section className={layout.sectionReverse}>
+    <div className={layout.sectionImgReverse}>
+      <img src={adm} alt="billing" className="w-[100%] h-[100%] relative z-[5]" />
+
+      {/* gradient start */}
+      <div className="absolute z-[3] -left-1/2 top-0 w-[50%] h-[50%] rounded-full white__gradient" />
+      <div className="absolute z-[0] w-[50%] h-[50%] -left-1/2 bottom-0 rounded-full pink__gradient" />
+      {/* gradient end */}
+    </div>
     <div className={layout.sectionInfo}>
     <h2 className={styles.heading2}>
       Administração
@@ -22,14 +31,6 @@ const Financial = () => {
     </div>
 
     </div>
-    <div className={layout.sectionImgReverse}>
-      <img src={notebook_mocukp} alt="billing" className="w-[100%] h-[100%] relative z-[5]" />
-
-      {/* gradient start */}
-      <div className="absolute z-[3] -left-1/2 top-0 w-[50%] h-[50%] rounded-full white__gradient" />
-      <div className="absolute z-[0] w-[50%] h-[50%] -left-1/2 bottom-0 rounded-full pink__gradient" />
-      {/* gradient end */}
-    </div>
   </section>
   <section className={layout.sectionReverse}>
     <div className={layout.sectionInfo}>
@@ -44,7 +45,7 @@ const Financial = () => {
     </p>
     </div>
     <div className={layout.sectionImgReverse}>
-      <img src={notebook_mocukp} alt="billing" className="w-[100%] h-[100%] relative z-[5]" />
+      <img src={almoxarifado} alt="billing" className="w-[100%] h-[100%] relative z-[5]" />
 
       {/* gradient start */}
       <div className="absolute z-[3] -left-1/2 top-0 w-[50%] h-[50%] rounded-full white__gradient" />

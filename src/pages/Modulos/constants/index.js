f5b69a6950c962e1a5cd03cf156e36348ff64545
flavioepimentel <flavio.ericksan@hotmail.com>
2023-12-05 12:06:53 -0300

    feat: :sparkles: Add component FetureCard

---
+++ src/pages/Modulos/constants/index.js
@@ -0,0 +1,25 @@
+import { integration, optimization, control  } from "../assets";
+
+export const features = [
+  {
+    id: "feature-1",
+    icon: control,
+    title: "Controle",
+    content:
+      "Com as informações fornecidas pelo módulo de abastecimento, escale as trocas de óleo e de filtros.",
+  },
+  {
+    id: "feature-2",
+    icon: integration,
+    title: "Integração",
+    content:
+      "Requisita os filtros que serão utlizados em cada equipamento automaticamente quando necessário.",
+  },
+  {
+    id: "feature-3",
+    icon: optimization,
+    title: "Otimização",
+    content:
+      "Gera um processo de aperfeiçoamento atráves dos planos de manutençao preventiva",
+  },
+];

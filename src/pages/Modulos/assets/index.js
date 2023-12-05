eaded412d8e4447cb49774f5aae238c9f1ba3926
flavioepimentel <flavio.ericksan@hotmail.com>
2023-12-04 21:08:28 -0300

    style: :bento: Export assets

--- src/pages/Modulos/assets/index.js
+++ src/pages/Modulos/assets/index.js
@@ -1,11 +1,22 @@
 import adm from "./adm.png";
 import almoxarifado from "./almoxarifado.png";
 import compras from "./compras.png";
+import custos from "./custos.png";
+import controle from "./controle.png";
+import postos from "./postos.png";
+import pneu from "./pneu.png";
+import abastecimento from "./abastecimento.png";
+import veiculos from "./veiculos.png";
 
 
 export {
   adm,
   almoxarifado,
-  compras
-
+  compras,
+  custos,
+  controle,
+  postos,
+  pneu,
+  abastecimento,
+  veiculos
 };

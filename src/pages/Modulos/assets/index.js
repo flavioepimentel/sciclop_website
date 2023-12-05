674b8a07939b9fbfc8896296bba9edc4ca1d44b4
flavioepimentel <flavio.ericksan@hotmail.com>
2023-12-05 12:09:58 -0300

    style: :lipstick: Export images to pages

--- src/pages/Modulos/assets/index.js
+++ src/pages/Modulos/assets/index.js
@@ -7,6 +7,14 @@ import postos from "./postos.png";
 import pneu from "./pneu.png";
 import abastecimento from "./abastecimento.png";
 import veiculos from "./veiculos.png";
+import integration from "./integration.svg";
+import optimization from "./optimization.svg";
+import control from "./control.svg";
+import lub from "./lub.png";
+import oficina from "./oficina.png";
+import lubrifica from "./lubrifica.png";
+import mecanica from "./mecanica.png";
+import rodante from "./rodante.png";
 
 
 export {
@@ -18,5 +26,13 @@ export {
   postos,
   pneu,
   abastecimento,
-  veiculos
+  veiculos,
+  integration,
+  optimization,
+  control, 
+  lub,
+  oficina,
+  lubrifica,
+  mecanica,
+  rodante
 };

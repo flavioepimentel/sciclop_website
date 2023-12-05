c9c980bc9701fda340a28502ac8c7118128c7085
flavioepimentel <flavio.ericksan@hotmail.com>
2023-12-05 16:33:32 -0300

    fix: :ambulance: Tentativa 04: Altera basename no componente BrowserRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom';
+import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider, Routes, BrowserRouter } from 'react-router-dom';
 import Layout from "./Layout/Layout";
 import Home from "./pages/Home/Home";
 import Financial from "./pages/Modulos/Financial";
@@ -7,25 +7,44 @@ import Maintenance from "./pages/Modulos/Maintenance";
 import About from "./pages/About/About";
 import NoPage from "./pages/NoPage/NoPage";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css';
 
 
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route exact path="/sciclop_website" element={<Layout />}>
-      <Route exact index element={<Home />} />
-      <Route exact path="about" element={<About />} />
-      <Route exact path="financial" element={<Financial />} />
-      <Route exact path="fleet" element={<Fleet />} />
-      <Route exact path="maintenance" element={<Maintenance />} />
-      <Route exact path="*" element={<NoPage />} />
-    </Route>
-  )
-)
-
-export default function App({routes}) {
+export default function App() {
   return (
-    <>
-      <RouterProvider router={router}/>
-    </>
+    <BrowserRouter basename="/app">
+      <Routes>
+        <Route exact path="/" element={<Layout />}>
+          <Route exact index element={<Home />} />
+          <Route exact path="about" element={<About />} />
+          <Route exact path="financial" element={<Financial />} />
+          <Route exact path="fleet" element={<Fleet />} />
+          <Route exact path="maintenance" element={<Maintenance />} />
+          <Route exact path="*" element={<NoPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
+
+// const router = createBrowserRouter(
+//   createRoutesFromElements(
+    
+//     <Route exact path="/sciclop_website" element={<Layout />}>
+//       <Route exact index element={<Home />} />
+//       <Route exact path="about" element={<About />} />
+//       <Route exact path="financial" element={<Financial />} />
+//       <Route exact path="fleet" element={<Fleet />} />
+//       <Route exact path="maintenance" element={<Maintenance />} />
+//       <Route exact path="*" element={<NoPage />} />
+//     </Route>
+//   )
+// )
+
+// export default function App({routes}) {
+//   return (
+//     <>
+//       <RouterProvider router={router}/>
+//     </>
+//   );
+// }

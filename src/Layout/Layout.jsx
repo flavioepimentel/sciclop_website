2368445506e89ff3cde007b7c50cb2fd7bbbfcc0
flavioepimentel <flavio.ericksan@hotmail.com>
2023-12-07 19:58:38 -0300

    style: :lipstick: Remove limitação do CTA

--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -5,11 +5,11 @@ import { Outlet, Link } from "react-router-dom";
 const Layout = () => (
   <div className=" w-full overflow-hidden">
     <div className={`bg-black-gradient  ${styles.flexCenter}`}>
-      <div className={` ${styles.boxWidth}`}>
+      <div className={`${styles.boxWidth}`}>
         <Navbar />
       </div>
     </div>
-    <div className={` ${styles.paddingX} ${styles.flexCenter}`}>
+    <div className={` ${styles.paddingX} `}>
       <div >
         <Outlet/>
         <Footer />

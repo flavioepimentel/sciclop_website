e253f268561c9eb3828ebf48e4af108b6e6a4b97
flavioepimentel <flavio.ericksan@hotmail.com>
2023-12-07 09:27:57 -0300

    style: :lipstick: Ajusta limite da tela para responsividade

--- src/style.js
+++ src/style.js
@@ -1,5 +1,6 @@
 const styles = {
-  boxWidth: "xl:max-w-[1280px] w-full",
+    
+  boxWidth: "w-full",
 
   heading2: "font-poppins font-semibold xs:text-[48px] text-[40px] text-black xs:leading-[76.8px] leading-[66.8px] w-full",
   paragraph: "font-poppins font-normal text-black text-[18px] leading-[30.8px]",
@@ -21,7 +22,7 @@ export const layout = {
 
   sectionImgReverse: `flex-1 flex ${styles.flexCenter} md:mr-10 mr-0 md:mt-0 mt-10 relative`,
   sectionImg: `flex-1 flex ${styles.flexCenter} md:ml-10 ml-0 md:mt-0 mt-10 relative`,
-
+  paddingText: `ml-6`,
   sectionInfo: `flex-1 ${styles.flexStart} flex-col`,
 };
 

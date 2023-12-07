0826193e9270ab77d39824a00d0a22be11472292
flavioepimentel <flavio.ericksan@hotmail.com>
2023-12-07 09:53:23 -0300

    style: :lipstick: Remove classe de estilo boxWidth e paddingX
    Remove classe de estilo boxWidth e paddingX para melhorar responsividade com telas grandes e desprender mais os elementos do site e o background.

--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -4,13 +4,13 @@ import { Outlet, Link } from "react-router-dom";
 
 const Layout = () => (
   <div className=" w-full overflow-hidden">
-    <div className={`bg-black-gradient ${styles.paddingX} ${styles.flexCenter}`}>
+    <div className={`bg-black-gradient  ${styles.flexCenter}`}>
       <div className={` ${styles.boxWidth}`}>
         <Navbar />
       </div>
     </div>
     <div className={` ${styles.paddingX} ${styles.flexCenter}`}>
-      <div className={`${styles.boxWidth}`}>
+      <div >
         <Outlet/>
         <Footer />
       </div>

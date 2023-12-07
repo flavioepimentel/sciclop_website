a7570da9e8765a90a1a39e51326267caf0a365e6
flavioepimentel <flavio.ericksan@hotmail.com>
2023-12-07 10:23:49 -0300

    refactor: :recycle: Recicla e atualiza arquivos de importações e constantes
    Recicla e atualiza arquivos de importações e constantes para se adequar a regra de arquitetura global vs local

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,15 +1,7 @@
-import bill from "./bill.svg";
-import card from "./1.svg";
 import logo from "./siclope_logo.svg";
 import logo_black from "./siclope_logo_black2.svg";
-import quotes from "./quotes.svg";
-import send from "./Send.svg";
-import shield from "./Shield.svg";
-import star from "./Star.svg";
 import menu from "./menu.svg";
 import close from "./close.svg";
-import arrowUp from "./arrow-up.svg";
-import discount from "./Discount.svg";
 import facebook from "./facebook.svg";
 import instagram from "./instagram.svg";
 import linkedin from "./linkedin.svg";
@@ -17,21 +9,13 @@ import twitter from "./twitter.svg";
 import icon  from "./favicon.ico";
 
 export {
-  bill,
-  card,
   logo,
   logo_black,
-  icon,
-  quotes,
-  send,
-  shield,
-  star,
   menu,
   close,
-  arrowUp,
-  discount,
   facebook,
   instagram,
   linkedin,
   twitter,
+  icon,
 };
--- src/constants/index.js
+++ src/constants/index.js
@@ -1,4 +1,4 @@
-import { facebook, instagram, linkedin, twitter, send, shield, star  } from "../assets";
+import { facebook, instagram, linkedin, twitter } from "../assets";
 
 export const navLinks = [
   { //	O que é o Siclope, contato, Descrição da dor.
@@ -25,76 +25,6 @@ export const dropdownLinks = [
     title: "Administrativo",
   }
 ]
-
-
-export const features = [
-  {
-    id: "feature-1",
-    icon: star,
-    title: "Benefícios",
-    content:
-      "Atingir o equilíbrio entre máxima produtividade e o custo mínimo, com gerenciamento instantâneo de variações em sua frota e suas causas.",
-  },
-  {
-    id: "feature-2",
-    icon: shield,
-    title: "Máxima Confiabilidade",
-    content:
-      "Nós nos concentramos em produzir um fluxo de informação com grande nível de precisão, com a segurança e confiabilidade.",
-  },
-  {
-    id: "feature-3",
-    icon: send,
-    title: "Informação em Tempo Real",
-    content:
-      "Acesse dados para decisões do dia a dia baseada em fatos para intervenções acertivas antes que produzam efeitos negativos, garantindo o melhor resultado.",
-  },
-];
-
-export const feedback = [
-  {
-    id: "feedback-1",
-    content:
-      "O tratamento correto da realidade, permitindo correção de rumos que trariam prejuízos, passa necessariamente por um sistema confiável de dados e o treinamento da equipe no uso desses dados.",
-    name: "Herman Jensen",
-    title: "Eng de Mantuenção",
-    img: 'people01',
-  },
-  {
-    id: "feedback-2",
-    content:
-    "Análise de lubrificantes? Nunca havia ouvido falar, agora sei que isso foi criado há muito tempo. É o exame de sangue do equipamento, previnindo as doenças do desgaste, trazendo redução no custo operacional.",
-    name: "Steve Mark",
-    title: "Coord. Financeiro",
-    img: 'people02',
-  },
-  {
-    id: "feedback-3",
-    content:
-      "Novas tecnologias e a correta utilização de antigas tecnologias, trazem soluções ótimas que reduzem custos operacionais de forma técnica, sem comprometer a produção, do equipamento ou da equipe.",
-    name: "Kenn Gallagher",
-    title: "Chefe de Operações",
-    img: 'people03',
-  },
-];
-
-export const stats = [
-  {
-    id: "stats-1",
-    title: "Usuários Ativos",
-    value: "3100+",
-  },
-  {
-    id: "stats-2",
-    title: "Empresas Clientes",
-    value: "200+",
-  },
-  {
-    id: "stats-3",
-    title: "Veiculos Monitorados",
-    value: "2300+",
-  },
-];
 
 export const footerLinks = [
   {
@@ -161,21 +91,3 @@ export const socialMedia = [
   },
 ];
 
-export const clients = [
-  {
-    id: "client-1",
-    logo: 'airbnb',
-  },
-  {
-    id: "client-2",
-    logo: 'binance',
-  },
-  {
-    id: "client-3",
-    logo: 'coinbase',
-  },
-  {
-    id: "client-4",
-    logo: 'dropbox',
-  },
-];

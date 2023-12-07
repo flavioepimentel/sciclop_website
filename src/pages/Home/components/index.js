e828e594e7d01e1ddbb2c0f47a43b9224709cd33
flavioepimentel <flavio.ericksan@hotmail.com>
2023-12-07 13:17:50 -0300

    refactor: :building_construction: Altera arquitetura de arquivos

---
+++ src/pages/Home/components/index.js
@@ -0,0 +1,13 @@
+import Billing from "./Billing";
+import Business from "./Business";
+import CardDeal from "./CardDeal";
+import Testimonials from "./Testimonials";
+import Hero from "./Hero";
+
+export {
+  Billing, 
+  Business, 
+  CardDeal, 
+  Testimonials, 
+  Hero
+};

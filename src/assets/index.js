d22ffe831614aafacc010ad719b9d2baa66c2ece
flavioepimentel <flavio.ericksan@hotmail.com>
2023-12-07 09:31:26 -0300

    refactor: :truck: Altera formato das importações
    Altera formato das importações para adequar a otimização

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,39 +1,24 @@
-import airbnb from "./airbnb.png";
-import notebook_mocukp from "./notebook_mocukp.png";
-import bill from "./bill.png";
-import binance from "./binance.png";
-import card from "./1.png";
-import coinbase from "./coinbase.png";
-import dropbox from "./dropbox.png";
-import logo from "./siclope_logo.png";
-import logo_black from "./siclope_logo_black2.png";
+import bill from "./bill.svg";
+import card from "./1.svg";
+import logo from "./siclope_logo.svg";
+import logo_black from "./siclope_logo_black2.svg";
 import quotes from "./quotes.svg";
 import send from "./Send.svg";
 import shield from "./Shield.svg";
 import star from "./Star.svg";
 import menu from "./menu.svg";
 import close from "./close.svg";
-import google from "./google.svg";
-import apple from "./apple.svg";
 import arrowUp from "./arrow-up.svg";
 import discount from "./Discount.svg";
 import facebook from "./facebook.svg";
 import instagram from "./instagram.svg";
 import linkedin from "./linkedin.svg";
 import twitter from "./twitter.svg";
-import people01 from "./people01.png";
-import people02 from "./people02.png";
-import people03 from "./people03.png";
 import icon  from "./favicon.ico";
 
 export {
-  airbnb,
-  notebook_mocukp,
   bill,
-  binance,
   card,
-  coinbase,
-  dropbox,
   logo,
   logo_black,
   icon,
@@ -43,15 +28,10 @@ export {
   star,
   menu,
   close,
-  google,
-  apple,
   arrowUp,
   discount,
   facebook,
   instagram,
   linkedin,
   twitter,
-  people01,
-  people02,
-  people03,
 };

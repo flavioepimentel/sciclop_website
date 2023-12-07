be91a04ccb8e495083a0b1841ef3e6cdf753c3ed
flavioepimentel <flavio.ericksan@hotmail.com>
2023-12-07 10:24:18 -0300

    refactor: :recycle:

---
+++ src/pages/Home/assets/index.js
@@ -0,0 +1,19 @@
+import bill from "./bill.svg";
+import card from "./1.svg";
+import quotes from "./quotes.svg";
+import send from "./Send.svg";
+import shield from "./Shield.svg";
+import star from "./Star.svg";
+import arrowUp from "./arrow-up.svg";
+import discount from "./Discount.svg";
+
+export {
+  bill,
+  card,
+  quotes,
+  send,
+  shield,
+  star,
+  arrowUp,
+  discount,
+};

beff5eed38e9d6141e51b95169e7bea4643d12a6
flavioepimentel <flavio.ericksan@hotmail.com>
2023-12-08 15:55:45 -0300

    style: :lipstick: Cria 3 novas classes de estilo
    Cria novas classes de estilo como boxWidthImage para delimitar o tamanho proporcional e responsivo das imagens. Altera as unidade de medidas das classes 'heading2' e 'paragraph' de px para rem. Altera o valor padrão de padding no eixo Y para py-0 . Cria a classe 'especialSectionReverse' para a primeira e a última seção de cada página. Cria a classe 'hiddenImageOnBig' para esconder elementos em telas de dimensões maiores.

--- src/style.js
+++ src/style.js
@@ -1,15 +1,16 @@
 const styles = {
     
   boxWidth: "w-full",
+  boxWidthImage:'w-[86%] h-[86%]',
 
-  heading2: "font-poppins font-semibold xs:text-[48px] text-[40px] text-black xs:leading-[76.8px] leading-[66.8px] w-full",
-  paragraph: "font-poppins font-normal text-black text-[18px] leading-[30.8px]",
+  heading2: "font-poppins font-semibold xs:text-[3rem] text-[2.5rem] text-black xs:leading-[4.8rem] leading-[4.175rem] w-full",
+  paragraph: "font-poppins font-normal text-black text-[1.125rem] leading-[1.925rem]",
 
   flexCenter: "flex justify-center items-center",
   flexStart: "flex justify-between items-start",
 
   paddingX: "sm:px-0 px-0",
-  paddingY: "sm:py-16 py-4",
+  paddingY: "sm:py-16 py-0",
   padding: "sm:px-16 px-6 sm:py-12 py-4",
 
   marginX: "sm:mx-16 mx-6",
@@ -19,14 +20,12 @@ const styles = {
 export const layout = {
   section: `flex md:flex-row flex-col ${styles.paddingY}`,
   sectionReverse: `flex md:flex-row flex-col-reverse ${styles.paddingY}`,
+  especialSectionReverse: `flex md:flex-row flex-col-reverse sm:py-16 pt-8 pb-0`,
 
   sectionImgReverse: `flex-1 flex ${styles.flexCenter} md:mr-10 mr-0 md:mt-0 mt-10 relative`,
   sectionImg: `flex-1 flex ${styles.flexCenter} md:ml-10 ml-0 md:mt-0 mt-10 relative`,
   hiddenImageOnSmall: `md:block hidden`,
-  hiddenImageOnBig: `sm:hidden block`,
-  
-  widthImage:`max-w-`,
-
+  hiddenImageOnBig: `md:hidden block`,
   paddingText: `ml-10`,
   sectionInfo: `flex-1 ${styles.flexStart} flex-col`,
 };

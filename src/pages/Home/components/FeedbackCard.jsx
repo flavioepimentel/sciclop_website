1b1d772939ab66f629762333d3300f2d9f70459f
flavioepimentel <flavio.ericksan@hotmail.com>
2023-12-09 11:24:24 -0300

    style: :lipstick: Reativa nome de depoimentos

--- src/pages/Home/components/FeedbackCard.jsx
+++ src/pages/Home/components/FeedbackCard.jsx
@@ -10,9 +10,9 @@ const FeedbackCard = ({ content, name, title, img }) => (
     <div className="flex flex-row">
       {/* <img src={img} alt={name} className="w-[48px] h-[48px] rounded-full" /> */}
       <div className="flex flex-col ml-4">
-        {/* <h4 className="font-poppins font-semibold text-[20px] leading-[32px] text-black">
+        <h4 className="font-poppins font-semibold text-[20px] leading-[32px] text-black">
           {name}
-        </h4> */}
+        </h4>
         <p className="font-poppins font-normal text-[16px] leading-[24px] text-black">
           {title}
         </p>

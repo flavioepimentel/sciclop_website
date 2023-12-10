ad3421cd4bb74824dcc507742420e135291a03e0
flavioepimentel <flavio.ericksan@hotmail.com>
2023-12-10 18:35:50 -0300

    feat: :construction: Add components to About page

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,7 +1,21 @@
+import { HowWeDo, WeAre, Public, Propusel } from './components/'
+import styles from "../../style";
+import  CTA  from "../../Layout/components/CTA"
 
 const About = () => {
-  return ( <h1>Sobre nós</h1>)
-
+  return ( 
+    <div className="w-full overflow-hidden">
+      <div className={` ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+        <Propusel/>
+        <WeAre/>
+        <HowWeDo/>
+        <Public/>
+        <CTA />
+        </div>
+      </div>
+    </div>
+  )
 };
 
 export default About;

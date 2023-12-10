ba1d5b36157c9fdf3a36b371b7c93af1fbcabcef
flavioepimentel <flavio.ericksan@hotmail.com>
2023-12-10 18:39:33 -0300

    feat: :construction: Create 'Public' component

---
+++ src/pages/About/components/Public.jsx
@@ -0,0 +1,33 @@
+import styles, { layout } from "../../../style";
+import { bill } from '../assets/'
+
+const Public = () => {
+    return (
+        <section id='public' className={layout.sectionReverse}>
+            <div className={layout.sectionImgReverse}>
+              <img src={bill} alt="billing" className={`${styles.boxWidthImageLarge}`} />
+
+              {/* gradient start */}
+              <div className="absolute z-[3] -left-1/2 top-0 w-[50%] h-[50%] rounded-full white__gradient" />
+              <div className="absolute z-[0] w-[50%] h-[50%] -left-1/2 bottom-0 rounded-full pink__gradient" />
+              {/* gradient end */}
+            </div>
+            <div className={`${layout.paddingText} ${layout.sectionInfo}`}>
+                <h1 className={styles.heading2}>A quem serve</h1>
+                <p className={`${styles.paragraph}`}>
+                    O Siclope atende a qualquer empresa com uma frota de equipamentos em sua operação. 
+                    Seja para veículos leves, como em empresas de entrega ou locadoras de automóveis, 
+                    ou para veículos de transporte de cargas e passageiros, como transporte de longa 
+                    distância, empresas de ônibus ou transporte urbano. Além disso, é útil para 
+                    prestadores de serviços, incluindo concreteiras, construção civil, terraplanagem, entre outros.
+
+                    Com aplicação também em companhias agrícolas, pedreiras, empresas de perfuração de poços artesianos, 
+                    o Siclope oferece uma solução para otimizar o custo operacional em qualquer cenário onde haja uma 
+                    frota de equipamentos produtivos.
+                </p>
+            </div>
+        </section>
+    )
+}
+
+export default Public

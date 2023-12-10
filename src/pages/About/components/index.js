63033282e8d68e2c1c21be26faae836fd6ca16d9
flavioepimentel <flavio.ericksan@hotmail.com>
2023-12-10 18:42:21 -0300

    feat: :sparkles: Add export index to components

---
+++ src/pages/About/components/index.js
@@ -0,0 +1,11 @@
+import Propusel from "./Propusel";
+import Public from './Public';
+import WeAre from './WeAre';
+import HowWeDo from './HowWeDo';
+
+export {
+    Propusel,
+    Public,
+    WeAre,
+    HowWeDo
+}

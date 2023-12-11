ed33bef7cdf9387eacbb5bea5aefa2b2e9c14e21
flavioepimentel <flavio.ericksan@hotmail.com>
2023-12-10 23:11:26 -0300

    style: :lipstick: Ajuste de espaçamento entre titulo e texto

--- src/pages/Home/components/CardDeal.jsx
+++ src/pages/Home/components/CardDeal.jsx
@@ -3,13 +3,13 @@ import styles, { layout } from "../../../style";
 import Button from "../../../Layout/components/Button";
 
 const CardDeal = () => (
-  <section className={`flex-1 flex justify-center items-start flex-col xl:px-0 sm:px-10 px-6 ${layout.sectionHome}`}>
+  <section className={`flex-1 flex justify-center items-start flex-col xl:px-0 sm:px-6 px-6 ${layout.sectionHome}`}>
     <div className={`${layout.paddingText} ${layout.sectionInfo}`}>
       <h2 className={styles.heading2}>
         Avalie as melhores marcas e fornecedores<br className="sm:block hidden" /> em alguns
         passos
       </h2>
-      <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+      <p className={`${styles.paragraph} mt-5`}>
         Escolha as marcas que demonstram o melhor desempenho na sua área de ação e 
         de empresas prestadoras de serviços que atendam aos níveis de qualidade ideais.
       </p>

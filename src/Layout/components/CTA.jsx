1484e698059405b835af0df554c5394495eea7c6
flavioepimentel <flavio.ericksan@hotmail.com>
2023-12-11 16:17:12 -0300

    chore: :art: Altera a forma de declarar a classe name

--- src/Layout/components/CTA.jsx
+++ src/Layout/components/CTA.jsx
@@ -3,7 +3,7 @@ import ButtonReverse from "./ButtonReverse";
 
 const CTA = () => (
   <section className={`${styles.flexCenter} ${styles.marginY} ${styles.padding} sm:flex-row flex-col bg-black-gradient-2 rounded-[20px] box-shadow`}>
-    <div className="flex-1 flex flex-col">
+    <div className={`flex-1 flex flex-col`}>
       <h2 className={`font-poppins font-semibold xs:text-[48px] text-[40px] text-white xs:leading-[76.8px] leading-[66.8px] w-full`}>Entre em contato agora!</h2>
       <p className={`${`"font-poppins font-normal text-white text-[18px] leading-[30.8px]"`} max-w-[470px] mt-5`}>
         Descubra tudo o que seu negócio precisa hoje para começar a economizar com uma gestão eficiente.

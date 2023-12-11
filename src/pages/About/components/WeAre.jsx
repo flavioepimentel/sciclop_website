678c90da2cb767d49b3504d40c2e7ab8506029e4
flavioepimentel <flavio.ericksan@hotmail.com>
2023-12-11 02:31:10 -0300

    style: :speech_balloon: Atualiza texto e aplica estilo para espaçamento

--- src/pages/About/components/WeAre.jsx
+++ src/pages/About/components/WeAre.jsx
@@ -5,7 +5,6 @@ const WeAre = () => {
     return (
         <section id="weare" className={layout.especialSectionReverse}>
         <div className={layout.sectionImgReverse}>
-          <img src={bill} alt="billing" className={`${styles.boxWidthImageLarge}`} />
         
           {/* gradient start */}
           <div className="absolute z-[3] -left-1/2 top-0 w-[50%] h-[50%] rounded-full white__gradient" />
@@ -16,21 +15,19 @@ const WeAre = () => {
             <h1 className={styles.heading2}>Quem somos</h1>
                 <p className={`${styles.paragraph}`}>
                     O Siclope é um sistema tipo ERP projetado para fornecer informações táticas e estratégicas, 
-                    integrando todos os módulos para administrar eficientemente a frota em tempo real, com máxima 
-                    confiabilidade. Desenvolvido por nossa equipe, o sistema prioriza a produção de informações 
-                    precisas, eliminando praticamente o uso de papel, e conta com a segurança e confiabilidade dos 
-                    servidores da Amazon.
-
-                    Uma vez geradas as informações, os tomadores de decisão passam por treinamento para utilizá-las, 
-                    buscando compreender suas origens. Dessa forma, as decisões diárias são embasadas em fatos, 
-                    administrando resultados positivos. O sistema atua na identificação e intervenção nas causas antes 
-                    que produzam efeitos negativos, evitando sua perpetuação por desconhecimento.
-
-                    Em um nível mais elevado, o Siclope fornece informações estratégicas, auxiliando na tomada de 
-                    decisões alinhadas ao objetivo de produção ao menor custo. Isso inclui a seleção de marcas com 
-                    melhor desempenho e a escolha de prestadores de serviços que atendam aos padrões ideais de 
-                    qualidade, visando à redução de custos.
-
+                    integrando módulos para administrar eficientemente a frota em tempo real. Desenvolvido por 
+                    nossa equipe, o sistema prioriza a produção de informações precisas, eliminando praticamente 
+                    o uso de papel.
+                </p>
+                <p className={`${styles.paragraph}`}>
+                    Em um nível mais elevado, o Siclope fornece informações estratégicas,
+                    isso inclui a seleção de marcas com melhor desempenho e a escolha de 
+                    prestadores de serviços que atendam aos padrões ideais de qualidade. 
+                    Com as informações geradas as decisões diárias são embasadas em fatos. 
+                    O sistema atua na identificação e intervenção nas causas antes que 
+                    produzam efeitos negativos.
+                </p>
+                <p className={`${styles.paragraph}`}>
                     Além disso, o sistema permite a segregação de informações específicas de testes, avaliando 
                     resultados operacionais e fornecendo dados relevantes para diversos departamentos, como financeiro, 
                     operacional, estratégico e administrativo.

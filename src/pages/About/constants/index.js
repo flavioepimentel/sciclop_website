8f16c8780aa8090c2ef61c98a1b9f5c8eae7b591
flavioepimentel <flavio.ericksan@hotmail.com>
2023-12-11 02:29:03 -0300

    feat: :sparkles: Implementa lista para página About

---
+++ src/pages/About/constants/index.js
@@ -0,0 +1,26 @@
+// import { integration, optimization, control  } from "../assets";
+
+export const features = [
+  {
+    id: "feature-1",
+    icon: 'fleet',
+    title: "Frota",
+    content:
+      "Baseado no tamanho da frota.",
+  },
+  {
+    id: "feature-2",
+    icon: 'time',
+    title: "Colaboradores",
+    content:
+      "Número de colaboradores envolvidos e treinados.",
+  },
+  {
+    id: "feature-3",
+    icon: 'optimization',
+    title: "Otimização",
+    content:
+      "Quantidade de módulos desejados.",
+  },
+
+];

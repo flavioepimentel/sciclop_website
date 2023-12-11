8b9527e9309a1267b87c124bdc49c2b4df52f634
flavioepimentel <flavio.ericksan@hotmail.com>
2023-12-11 02:25:16 -0300

    style: :lipstick: Correção de espaçamentos entre titulo e texto

--- src/pages/Modulos/Maintenance.jsx
+++ src/pages/Modulos/Maintenance.jsx
@@ -16,11 +16,11 @@ const Maintenance = () => {
       <div className="absolute z-[0] w-[50%] h-[50%] -left-1/2 bottom-0 rounded-full pink__gradient" />
       {/* gradient end */}
     </div>
-    <div className={`${layout.sectionInfoCTA}`}>
+    <div className={`${layout.sectionInfoCenter}`}>
     <h2 className={styles.heading2}>
       Pneus
       </h2>
-    <p className={`${styles.paragraph}`}>
+    <p className={`${styles.paragraph} mb-5`}>
       Um componente significativo no custo operacional, agora podem ser gerenciados de maneira inteligente. 
       Nosso módulo exclusivo não apenas controla o desempenho dos pneus, mas também fornece dados cruciais para escolher 
       o item ideal para cada aplicação, visando sempre o menor custo operacional padrão.
@@ -29,8 +29,7 @@ const Maintenance = () => {
     </div>
   </section>
   <section className={`flex md:flex-row flex-col-reverse sm:py-16 py-0`}>
-    <div className={layout.sectionInfo}>
-    <div className={`${layout.sectionImg} flex-col`}>
+    <div className={`${layout.paddingText} ${layout.sectionInfoCenter}`}>
     <h2 className={styles.heading2}>
       Mecânica
       </h2>
@@ -38,7 +37,6 @@ const Maintenance = () => {
       Este módulo é seu aliado ao criar salas especializadas para desmontagem, análise de falhas, retífica e testes de motores, ou para a manutenção de sistemas 
       hidráulicos, incluindo comandos, transmissões e conversores de torque. Oferecemos recursos abrangentes para registrar e controlar todo o processo.
     </p>
-    </div>
     </div>
     <div className={layout.sectionImgReverse}>
       <img src={mecanica} alt="billing" className={`${styles.boxWidthImage}`} />
@@ -58,8 +56,7 @@ const Maintenance = () => {
       <div className="absolute z-[0] w-[50%] h-[50%] -left-1/2 bottom-0 rounded-full pink__gradient" />
       {/* gradient end */}
     </div>
-    <div className={layout.sectionInfo}>
-    <div className={`${layout.sectionImg} flex-col`}>
+    <div className={`${layout.paddingText} ${layout.sectionInfoCenter}`}>
     <h2 className={styles.heading2}>
       Material rodante
       </h2>
@@ -67,7 +64,6 @@ const Maintenance = () => {
       Registre medições e acompanhe os desgastes, previna-se de problemas, 
       nosso módulo auxiliarão o atingimento de um maior do número de horas trabalhadas sem paradas desnecessárias.
     </p>
-    </div>
     </div>
   </section> 
   <CTA />

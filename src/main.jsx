92b69adede5c585e6b76617a9ebf440bf7e36c81
flavioepimentel <flavio.ericksan@hotmail.com>
2023-12-12 20:42:47 -0300

    perf: :zap: Implementa script para não bloquear scroll

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,20 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 
+function onScroll (evt) {
+
+    // Store the scroll value for laterz.
+    lastScrollY = window.scrollY;
+
+    // Prevent multiple rAF callbacks.
+    if (scheduledAnimationFrame)
+    return;
+
+    scheduledAnimationFrame = true;
+    requestAnimationFrame(readAndUpdatePage);
+}
+
+window.addEventListener('scroll', onScroll);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>

4b8aea548d17b423d2a5acefc09942db2c6a4e28
flavioepimentel <flavio.ericksan@hotmail.com>
2023-12-17 12:08:17 -0300

    feat: :sparkles: Add LoginButton as independent component

---
+++ src/Layout/components/LoginButton.jsx
@@ -0,0 +1,17 @@
+
+
+const LoginButton =() => {
+    return (
+        <>
+        <a href="https://frotas-erp.bubbleapps.io/version-test/">
+		    <button
+		    	key={"login"}
+		    	className={`font-poppins font-normal cursor-pointer text-[0.9rem] text-white mx-6 login type1`}
+		    >
+		    </button>
+		</a>
+        </>
+    )
+}
+
+export default LoginButton;

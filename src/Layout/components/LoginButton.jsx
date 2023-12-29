d958fa36da8acfbf4d2f1bb80a8d093ecff566b2
flavioepimentel <flavio.ericksan@hotmail.com>
2023-12-28 22:04:32 -0300

    feat: :sparkles: Implementa alerta com acessos

--- src/Layout/components/LoginButton.jsx
+++ src/Layout/components/LoginButton.jsx
@@ -1,16 +1,30 @@
 import "./LoginButton.css"
 import "../style.css"
+import Swal from 'sweetalert2'
+
+function alertUser() {
+	return Swal({
+  title: 'Utilize o acesso:',
+  text: `Usuário: 'visitante' e Senha: 'siclope'`,
+  icon: "success",
+  showCancelButton:true,	
+  showConfirmButton: true,
+  reverseButtons:true,
+  cancelButtonText:'Cancelar',
+  confirmButtonText: `<a href="https://frotas-erp.bubbleapps.io/version-test/">Conhecer agora!</a>`,
+  customClass: 'sweetalert'
+});
+}
 
 const LoginButton =() => {
     return (
         <>
-        <a href="https://frotas-erp.bubbleapps.io/version-test/">
 		    <button
-		    	key={"login"}
+		    	key={"login"} 
+				onClick={alertUser}
 		    	className={`font-poppins font-normal cursor-pointer text-[0.9rem] text-white mx-6 login type1`}
 		    >
 		    </button>
-		</a>
         </>
     )
 }

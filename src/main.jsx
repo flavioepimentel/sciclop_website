f95b2fcea2cb6ef98fea1d360c6a3a8086096e2f
flavioepimentel <flavio.ericksan@hotmail.com>
2023-12-28 22:03:43 -0300

    refactor: :poop: Comentando código com erro

--- src/main.jsx
+++ src/main.jsx
@@ -3,20 +3,20 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 
-function onScroll (evt) {
+// function onScroll (evt) {
 
-    // Store the scroll value for laterz.
-    lastScrollY = window.scrollY;
+//     // Store the scroll value for laterz.
+//     lastScrollY = window.scrollY;
 
-    // Prevent multiple rAF callbacks.
-    if (scheduledAnimationFrame)
-    return;
+//     // Prevent multiple rAF callbacks.
+//     if (scheduledAnimationFrame)
+//     return;
 
-    scheduledAnimationFrame = true;
-    requestAnimationFrame(readAndUpdatePage);
-}
+//     scheduledAnimationFrame = true;
+//     requestAnimationFrame(readAndUpdatePage);
+// }
 
-window.addEventListener('scroll', onScroll);
+// window.addEventListener('scroll', onScroll);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
